b008d112714b26ade7d2ad92b9cd7236adabcad9
Ashley <a.bloskas@yahoo.com>
2018-04-17 13:11:58 -0700

    made changes again

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,15 +6,9 @@ class UsersController < ApplicationController
         @user = User.new(name:params[:name], email:params[:email], password:params[:password], password_confirmation:params[:confirm])
         if @user.save
             session[:user_id] = @user.id
-<<<<<<< HEAD
             redirect_to "/sessions/new"
         else 
             flash[:errors] = ["can't be blank"] 
-=======
-            redirect_to "users/#{@user.id}"
-        else 
-            flash[:errors] = ["Invalid"] 
->>>>>>> 87b2f7d466574525a47b81e3a452363574748fb1
             redirect_to '/users/new'
         end
     end
