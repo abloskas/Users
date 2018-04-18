a8e1d1482fe06daa2b8e2d314140533f8d8b7b0c
Ashley <a.bloskas@yahoo.com>
2018-04-18 11:51:04 -0700

    added authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,10 @@ class ApplicationController < ActionController::Base
     User.find(session[:user_id]) if session[:user_id]
   end
   helper_method :current_user
+
+  def require_login
+    if session[:user_id] == nil
+      redirect_to "/sessions/new"
+    end
+  end
 end
